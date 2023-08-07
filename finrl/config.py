18fe03d070305d7400b44d683e501d86c0416e78
mbkizil <63160324+mbkizil@users.noreply.github.com>
2023-08-08 00:25:27 +0300

    Update config.py

--- finrl/config.py
+++ finrl/config.py
@@ -58,6 +58,12 @@ ERL_PARAMS = {
     "eval_times": 64,  # bug fix:KeyError: 'eval_times' line 68, in get_model model.eval_times = model_kwargs["eval_times"]
 }
 RLlib_PARAMS = {"lr": 5e-5, "train_batch_size": 500, "gamma": 0.99}
+REC_PARAMS = {
+    "n_steps": 2048,
+    "ent_coef": 0.01,
+    "learning_rate": 0.00025,
+    "batch_size": 64,
+}
 
 
 # Possible time zones
